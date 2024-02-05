cec41a94eaa57b3cab95c9091d846295707022a9
Leonardo <leonardo38361@gmail.com>
2024-02-05 02:00:59 -0300

    learning about mathematical operations and data types...

---
+++ 20-typeErrorCheckingConversion.py
@@ -0,0 +1,14 @@
+# num_char = len(input("What is your name? "))
+# print("Your name has " + num_char + " characters.")
+# new_num_char = str(num_char);
+
+# print(type(new_num_char))
+
+a = 123
+print(type(a))
+
+# print(70 + float("100.5"))
+stringConcatenate = str(70) + str(100)
+print(type(stringConcatenate))
+
+print(( 3 * 3 ) + ( 3 / 3 ) -3)
