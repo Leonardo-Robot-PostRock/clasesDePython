b4ff3283a438c4466f475b4fc246a90335275171
Leonardo Puebla <leonardo38361@gmail.com>
2024-11-04 17:22:07 -0300

    ADD: number manipulation and f-strings

---
+++ day2/task.py
@@ -0,0 +1,22 @@
+height = 1.65 
+weight = 84
+
+bmi = weight / (height ** 2)
+
+print(bmi)
+
+print(round(bmi, 2))
+
+# Assignment Operator
+
+score = 0
+
+score += 1
+
+print(score)
+
+#f-strings
+
+# In python, we can use f-strings to insert a variable or an expression into a string.
+
+print(f"Your score is {score}, your height is {height}")
